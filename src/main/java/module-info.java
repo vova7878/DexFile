493f1044af2b26cec9ee2e988963fcb9a572c8c4
v7878 <46866705+vova7878@users.noreply.github.com>
2024-08-09 22:35:20 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,4 +14,6 @@ module com.v7878.dex {
     exports com.v7878.dex.immutable.debug;
     exports com.v7878.dex.immutable.value;
     exports com.v7878.dex.immutable.bytecode;
+
+    requires com.v7878.misc;
 }
