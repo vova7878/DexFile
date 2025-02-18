368afe67d29236c0febf3717113773d39e8b5189
v7878 <46866705+vova7878@users.noreply.github.com>
2025-02-18 19:42:45 +0700

    Remove unnecessary CatchHandler.equals and hashCode methods

--- src/main/java/com/v7878/dex/raw/CatchHandler.java
+++ src/main/java/com/v7878/dex/raw/CatchHandler.java
@@ -3,20 +3,6 @@ package com.v7878.dex.raw;
 import com.v7878.dex.immutable.ExceptionHandler;
 
 import java.util.List;
-import java.util.Objects;
 
 public record CatchHandler(List<ExceptionHandler> elements, Integer catch_all_addr) {
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        //noinspection DeconstructionCanBeUsed
-        return obj instanceof CatchHandler that
-                && Objects.equals(catch_all_addr, that.catch_all_addr)
-                && Objects.equals(elements, that.elements);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(catch_all_addr, elements);
-    }
 }
