aa4acc9bfd0f303b999799320240b7cd106d8dd3
v7878 <46866705+vova7878@users.noreply.github.com>
2025-10-18 21:51:02 +0700

    Add DexIO.readDexContainerCache

--- src/main/java/com/v7878/dex/DexIO.java
+++ src/main/java/com/v7878/dex/DexIO.java
@@ -15,6 +15,7 @@ import com.v7878.dex.io.ByteArrayInput;
 import com.v7878.dex.raw.DexReader;
 import com.v7878.dex.raw.DexWriter;
 import com.v7878.dex.raw.SharedData;
+import com.v7878.dex.util.EmptyArrays;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -157,11 +158,20 @@ public final class DexIO {
         }
     }
 
-    public static Dex read(ReadOptions options, byte[] data,
-                           int data_offset, int header_offset, int[] ids) {
+    public static DexReaderCache readCache(ReadOptions options, byte[] data,
+                                           int data_offset, int header_offset) {
         Objects.requireNonNull(options);
         var input = new ByteArrayInput(data).duplicateAt(data_offset).markAsStart();
-        var reader = new DexReader(options, input, header_offset);
+        return new DexReader(options, input, header_offset);
+    }
+
+    public static DexReaderCache readCache(byte[] data) {
+        return readCache(ReadOptions.defaultOptions(), data, 0, 0);
+    }
+
+    public static Dex read(ReadOptions options, byte[] data,
+                           int data_offset, int header_offset, int[] ids) {
+        var reader = readCache(options, data, data_offset, header_offset);
         var classes = reader.getClasses();
         if (ids == null) {
             return Dex.of(classes);
@@ -182,20 +192,7 @@ public final class DexIO {
         return read(ReadOptions.defaultOptions(), data, 0, 0);
     }
 
-    public static DexReaderCache readCache(ReadOptions options, byte[] data,
-                                           int data_offset, int header_offset) {
-        Objects.requireNonNull(options);
-        var input = new ByteArrayInput(data).duplicateAt(data_offset).markAsStart();
-        return new DexReader(options, input, header_offset);
-    }
-
-    public static DexReaderCache readCache(byte[] data) {
-        return readCache(ReadOptions.defaultOptions(), data, 0, 0);
-    }
-
-    //TODO: public static Dex[] readDexContainer(ReadOptions options, byte[] data, int[][] ids) {}
-
-    public static Dex[] readDexContainer(ReadOptions options, byte[] data, int data_offset) {
+    public static DexReaderCache[] readDexContainerCache(ReadOptions options, byte[] data, int data_offset) {
         Objects.requireNonNull(options);
         var input = new ByteArrayInput(data).duplicateAt(data_offset).markAsStart();
         var readers = new ArrayList<DexReader>();
@@ -205,9 +202,16 @@ public final class DexIO {
             readers.add(reader);
             header_offset += reader.getFileSize();
         }
-        var out = new Dex[readers.size()];
+        return readers.toArray(EmptyArrays.DEX_READER_CACHE);
+    }
+
+    //TODO: public static Dex[] readDexContainer(ReadOptions options, byte[] data, int[][] ids) {}
+
+    public static Dex[] readDexContainer(ReadOptions options, byte[] data, int data_offset) {
+        var readers = readDexContainerCache(options, data, data_offset);
+        var out = new Dex[readers.length];
         for (int i = 0; i < out.length; i++) {
-            out[i] = Dex.of(readers.get(i).getClasses());
+            out[i] = Dex.of(readers[i].getClasses());
         }
         return out;
     }
