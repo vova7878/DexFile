72fe022d748a6cb48db289b2264cbdec02032e22
v7878 <46866705+vova7878@users.noreply.github.com>
2025-11-04 22:37:33 +0700

    Update build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,12 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "DexFile"
