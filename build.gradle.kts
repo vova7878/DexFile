61747c3ee95771e74985b70a8c24768134223b10
v7878 <46866705+vova7878@users.noreply.github.com>
2025-11-04 22:52:39 +0700

    Set up publications to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,42 @@ dependencies {
     implementation(libs.android.misc)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("javaMaven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = false)
+    signAllPublications()
 
-            from(components["java"])
+    coordinates(
+        groupId = "io.github.vova7878",
+        artifactId = "DexFile",
+        version = project.version.toString()
+    )
+
+    pom {
+        name.set("DexFile")
+        description.set("Library for reading and writing dex files")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vova7878/DexFile")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("repository")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("vova7878")
+                name.set("Vladimir Kozelkov")
+                url.set("https://github.com/vova7878")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/vova7878/DexFile")
+            connection.set("scm:git:git://github.com/vova7878/DexFile.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vova7878/DexFile.git")
         }
     }
 }
