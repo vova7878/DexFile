4d94bfc10f0c598630fcbf1ee3da1d5fb1cbb60d
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2026-04-30 15:05:00 +0700

    Fix some typos x2

--- src/main/java/com/v7878/dex/analysis/AnalyzedMethod.java
+++ src/main/java/com/v7878/dex/analysis/AnalyzedMethod.java
@@ -1482,11 +1482,10 @@ public final class AnalyzedMethod {
                     if (Objects.equals(type.exactType(), ref)) {
                         is_nnop = false;
                         is_nop = true;
-                    } else //noinspection ConstantValue
-                        if (false/*!TypeResolver._instanceOf(resolver, type, ref, true)*/) {
-                        // TODO: There are some check-cast cases that can never be true.
-                        //  For example, [I to [B. But I haven't yet understood the general
-                        //  principles for detecting them.
+                    } else if (!TypeResolver._instanceOf(resolver, type, ref, true) &&
+                            !TypeResolver._instanceOf(resolver, ref, type, true)) {
+                        // Mutually incompatible types.
+                        // i.e. Integer and String or Object[] and int[] etc.
                         is_nnop = false;
                         is_nop = false;
                         next_reachable = false;
